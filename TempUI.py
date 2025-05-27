4f6d6f236a8294fe08664359fb5241e986141b26
Grace Liu <graceshiliu@gmail.com>
2025-05-27 16:39:30 -0700

    Add all (unformatted) components

--- TempUI.py
+++ TempUI.py
@@ -22,8 +22,11 @@ root.geometry(f"{dynamic_width}x{dynamic_height}+{x}+{y}")
 
 
 
-frm = ttk.Frame(root, padding=10)
-frm.grid()
-ttk.Label(frm, text="Hello World!").grid(column=0, row=0)
-ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=0)
+frm = Frame(root, highlightbackground="black", highlightthickness=2)
+frm.pack(padx=10, pady=10, fill=BOTH, expand=True)
+ttk.Label(frm, text="Welcome to Prent!").pack()
+ttk.Label(frm, text="To get started, please upload an image or video file.").pack()
+ttk.Button(frm, text="Browse Files").pack()
+ttk.Button(frm, text="Help").pack()
+ttk.Button(frm, text="About").pack()
 root.mainloop()
