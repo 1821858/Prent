8055b79c3686d5d02c7278a301e3e1e677aae7a8
Grace Liu <graceshiliu@gmail.com>
2025-05-27 18:13:35 -0700

    Fix about page text

--- frontend/About.py
+++ frontend/About.py
@@ -11,17 +11,17 @@ def create_about_page(parent, show_home_callback):
     style = ttk.Style()
     style.configure("Home.TButton", font=body_font)
 
-    help_text = (
+    about_text = (
         "Prent is an image and video editing software that uses OpenCV to process files and apply effects.\n\n"
         "Currently, the available effects include polygonize, cartoon, and sketch, but stay tuned for more effects to come.\n\n"
         "Prent was made by users Curiousfish and Grackfish as a personal project. View our growing GitHub repository here!"
     )
 
-    ttk.Label(main_frame, text="Help", font=header_font).pack(pady=[35, 0])
+    ttk.Label(main_frame, text="About", font=header_font).pack(pady=[35, 0])
 
     body_frame = Frame(main_frame)
     body_frame.pack(fill=X)
-    ttk.Label(body_frame, text=help_text, wraplength=1050, justify=LEFT, font=body_font).pack(pady=25)
+    ttk.Label(body_frame, text=about_text, wraplength=1050, justify=LEFT, font=body_font).pack(pady=25)
 
     ttk.Button(main_frame, text="Back", command=show_home_callback, style="Home.TButton").pack(ipadx=10, ipady=10)
 
